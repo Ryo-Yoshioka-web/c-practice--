b8b5d805d276265806389e3db3411091e82546a9
Ryo yoshioka <ryou.y.0515@gmail.com>
2023-02-02 01:16:10 +0900

    復習

---
+++ review/review.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+// 2-5
+//int main() {
+//	int x, y, temp;
+//	scanf_s("%d %d", &x, &y);
+//	temp = x;
+//	x = y;
+//	y = temp;
+//	printf("x=%d y=%d", x, y);
+//}
+
+// 4-13
+//int main() {
+//	int i;
+//	scanf_s("%d", i);
+//	switch (i)
+//	{
+//	case 1: printf("1");
+//		break;
+//	case 2:
+//	default:
+//		break;
+//	}
+//}
+
+// 5-3
+//int main() {
+//	int i, num;
+//	num = 1;
+//	for ( i = 1; i <= 8; i++)
+//	{
+//		printf("%d\n",num *= 2);
+//	}
+//}
+
+// 5-4
+//int main() {
+//	int i;
+//	int num;
+//	int value = 1;
+//	scanf_s("%d", &num);
+//	for ( i = 2; i <= num ; i++)
+//	{
+//		value *= i;
+//	}
+//	printf("%d\n", value);
+//}
+
+// 5-4
+//int main() {
+//	int x = 1;
+//	int i;
+//
+//	for (i = 2; i <= 7; i++)
+//		x *= i;
+//
+//	printf("%d", x);
+//}
+
+// 5-16
+//int	main() {
+//    int value;
+//    int i;
+//
+//    scanf_s("%d", &value);
+//    for (i = 2; i <= (value / 2); i++)
+//    {
+//        if ((value % i) == 0)
+//            break;
+//    }
+//
+//    if (i > (value / 2))
+//        printf("%d‚Í‘f”‚Å‚·", value);
+//    else
+//        printf("%d‚Í‘f”‚Å‚Í‚ ‚è‚Ü‚¹‚ñ", value);
+//}
+
+//5-17
+int main() {
+
+}
