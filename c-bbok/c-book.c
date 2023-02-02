67fd49eb141e789f07f29edb3a449cd6ec699b4e
Ryo yoshioka <ryou.y.0515@gmail.com>
2023-02-03 04:06:52 +0900

    第5章完了

--- c-bbok/c-book.c
+++ c-bbok/c-book.c
@@ -60,3 +60,105 @@ typedef char String[1024];
 //		}
 //	}
 //}
+
+//5-3
+//int main() {
+//	int num;
+//	printf("1～9の数を入力してください\n");
+//	scanf_s("%d", &num);
+//	switch (num)
+//	{
+//	case 1:
+//	case 2: 
+//		printf("バッテリ-");
+//		break;
+//	case 3:
+//	case 4:
+//	case 5:
+//	case 6:
+//		printf("内野手");
+//		break;
+//	case 7:
+//	case 8:
+//	case 9:
+//		printf("外野手");
+//		break;
+//	default:
+//		printf("入力された守備位置はありません");
+//		break;
+//	}
+//}
+
+//int main() {
+//	int num;
+//	printf("1～9の数を入力してください\n");
+//	scanf_s("%d", &num);
+//	if (num == 1 || num == 2)
+//	{
+//		printf("バッテリ-");
+//	}
+//	else if (num == 3 || num == 4 || num == 5 || num == 6)
+//	{
+//		printf("内野手");
+//	}
+//	else if (num == 7 || num == 8 || num == 9)
+//	{
+//		printf("外野手");
+//	}
+//	else
+//	{
+//		printf("入力された守備位置はありません");
+//	}
+//}
+
+//int main() {
+//	int i;
+//	int apple = 120;
+//	int mikan = 400;
+//	int money = 3000;
+//	printf("リンゴ ");
+//	for ( i = 0; apple * i < money; i++)
+//	{
+//		printf("*");
+//	}
+//	printf(" 余り%d円\n", money - (apple * i));
+//
+//	printf("みヵん ");
+//	for (i = 0; mikan * i < money; i++)
+//	{
+//		printf("*");
+//	}
+//	printf(" 余り%d円\n", money - (mikan * i - mikan) );
+//}
+
+//int main() {
+//	int i;
+//	int answer;
+//	int num;
+//	answer = rand() % 10;
+//	printf("***数あてゲーム(レベル1)***\n回答のチャンスは4回までです\n1桁の数を入力してください:\n");
+//
+//	for ( i = 0; i < 4; i++)
+//	{
+//		scanf_s("%d", &num);
+//		if (answer == num)
+//		{
+//			printf("当たりました %d回目です\n", i +1);
+//			break;
+//		}
+//		else
+//		{
+//			if (answer < num)
+//			{
+//				printf("入力された数の方が大きいです\n");
+//			}
+//			else
+//			{
+//				printf("入力された数の方が小さいです\n");
+//			}
+//		}
+//	}
+//	if (answer != num) {
+//		printf("正解は%dです", answer);
+//	}
+//}
